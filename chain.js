2a797f87517138a6a6535d3ed600b813bcc67d01
neeldikshu <72940496+neeldikshu@users.noreply.github.com>
2020-12-17 19:16:34 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,34 @@
+class Chain
+{
+    constructor(body1,body2,offsetX,offsetY)
+    {
+      this.offsetX=offsetX
+      this.offsetY=offsetY
+
+      var option={
+          bodyA:body1,
+          bodyB:body2,
+          pointB:{x:this.offsetX,y:this.offsetY}
+        }
+
+        this.chain=Constraint.create(option)
+        World.add(world,this.chain)
+    }
+
+
+display()
+{
+    var pointA=this.chain.bodyA.position;
+    var pointB=this.chain.bodyB.position;
+
+    strokeWeight(2);
+    stroke("white")
+    var x1=pointA.x
+    var y1=pointA.y
+
+    var x2=pointB.x+this.offsetX
+    var y2=pointB.y+this.offsetY
+
+    line(x1,y1,x2,y2)
+}
+}
